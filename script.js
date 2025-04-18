6e92234000f61dfcd9b507f487453e42b216fb3d
DevIgorCruz <devigorcruz@gmail.com>
2025-04-18 05:20:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("meuBotao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+    mensagem.textContent = "''Não tenha medo de começar do zero, até os melhores programadores um dia escreveram sua primeira linha de código. Persistência transforma curiosidade em profissão.'' ";
+});
